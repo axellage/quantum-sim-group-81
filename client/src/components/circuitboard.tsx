fcf83f10e00a88fbcd97ad583e5a7179f0b85e9e
aalexpersson <alex.persson0408@gmail.com>
2024-02-06 18:48:06 +0100

    added gate components

---
+++ client/src/components/circuitboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './circuitboard.css';
+
+function Circuitboard() {
+    return (
+      <div className="Circuitboard">
+       
+      </div>
+    );
+  }
+  
+  export default Circuitboard;
