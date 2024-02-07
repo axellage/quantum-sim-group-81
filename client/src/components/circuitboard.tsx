9f65b59cdad6d00ea4e8df12d43288115c4c7413
Alexander Persson <alex.persson0408@gmail.com>
2024-02-07 15:55:20 +0100

    added remove button to remove qubits and changed variable names

--- client/src/components/circuitboard.tsx
+++ client/src/components/circuitboard.tsx
@@ -2,28 +2,28 @@ import React, { useState, ReactNode, useEffect } from 'react';
 import './circuitboard.css';
 
 function Circuitboard() {
-  const [ketLines, setKetLines] = useState<ReactNode[]>([]);
+  const [qubitLines, setQubitLines] = useState<ReactNode[]>([]);
 
   useEffect(() => {
     // Initialize ketLines with three elements when the component mounts
-    setKetLines([
-      <div className="ket-line" data-testid="qubit-line" key={0}>
+    setQubitLines([
+      <div className="qubit-line" data-testid="qubit-line" key={0}>
         <p>|0⟩</p><hr/>
       </div>,
-      <div className="ket-line" data-testid="qubit-line" key={1}>
+      <div className="qubit-line" data-testid="qubit-line" key={1}>
         <p>|0⟩</p><hr/>
       </div>,
-      <div className="ket-line" data-testid="qubit-line" key={2}>
+      <div className="qubit-line" data-testid="qubit-line" key={2}>
         <p>|0⟩</p><hr/>
       </div>
     ]);
   }, []); // Empty dependency array to ensure this effect runs only once, on mount
 
-  const handleClick = () => {
-    if (ketLines.length < 10) {
-      setKetLines(prevKetLines => [
-        ...prevKetLines,
-        <div className="ket-line" data-testid="qubit-line" key={prevKetLines.length}>
+  const addQubit = () => {
+    if (qubitLines.length < 10) {
+      setQubitLines(prevQubitLines => [
+        ...prevQubitLines,
+        <div className="qubit-line" data-testid="qubit-line" key={prevQubitLines.length}>
           <p>|0⟩</p><hr/>
         </div>
       ]);
@@ -34,12 +34,23 @@ function Circuitboard() {
     }
   };
 
+  const removeQubit = () => {
+    if (qubitLines.length > 0) {
+      setQubitLines(prevQubitLines => prevQubitLines.slice(0, -1));
+    }
+    else {
+      //TODO make this a visible error
+      console.log("Already 0 qubits");
+    }
+  };
+
   return (
     <div className="Circuitboard">
       <section className="circuit">
-        {ketLines}
+        {qubitLines}
       </section>
-      <button onClick={handleClick}>+</button>
+      <button onClick={addQubit}>+</button>
+      <button onClick={removeQubit}>-</button>
     </div>
   );
 }
