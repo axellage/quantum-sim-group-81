1105ac2c919d1d76e2cdb175da3e300fbe8b9306
Axel Bergman <axelbergman01@icloud.com>
2024-02-07 18:15:10 +0100

    backend: added ping endpoint for helping with frontends api implementation

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -40,7 +40,34 @@ fn simulate_circuit_handler(incoming_data: Json<IncomingData>) -> Json<OutgoingD
     Json(response)
 }
 
+#[derive(Serialize, Deserialize)]
+
+struct PingMessage {
+    message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+
+struct PingResponse {
+    message: String,
+}
+
+#[post("/ping", format = "json", data = "<ping_message>")]
+fn ping_handler(ping_message: Json<PingMessage>) -> Json<PingResponse> {
+    let data: PingMessage = ping_message.into_inner();
+
+    return if data.message == "ping" {
+        Json(PingResponse {
+            message: "pong".parse().unwrap(),
+        })
+    } else {
+        Json(PingResponse {
+            message: "Huh?".parse().unwrap(),
+        })
+    };
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![simulate_circuit_handler])
+    rocket::build().mount("/", routes![simulate_circuit_handler, ping_handler])
 }
