c3c78ed9b66317435cca0a2c6bd85963a27880a4
Axel Bergman <axelbergman01@icloud.com>
2024-02-08 20:51:39 +0100

    backend: refactored, optimized quantum gates

--- backend/src/simulation/quantum_state.rs
+++ backend/src/simulation/quantum_state.rs
@@ -1,8 +1,6 @@
 use crate::simulation::quantum_gate::QuantumGate;
 
-use ndarray::linalg::kron;
-use ndarray::{arr2, Array2};
-use num::integer::Roots;
+use ndarray::Array2;
 use num::{Complex, ToPrimitive};
 use serde::{Deserialize, Serialize};
 
@@ -33,28 +31,15 @@ impl QuantumState {
         self.col.len().ilog2().to_usize().unwrap()
     }
 
-    #[deprecated(note = "Use QuantumState::new(1) instead")]
-    #[cfg(test)]
-    pub fn ket_zero() -> QuantumState {
-        QuantumState::new(1)
-    }
-
-    #[deprecated(note = "Use QuantumState::new(1).apply_gate(QuantumGate::x_gate(), 0) instead")]
-    #[cfg(test)]
-    pub fn ket_one() -> QuantumState {
-        QuantumState::new(1).apply_gate(QuantumGate::x_gate())
-    }
-
-    // Apply the Kronecker product to two QuantumStates
-    pub fn kronecker(self, other: &QuantumState) -> QuantumState {
-        QuantumState {
-            col: kron(&self.col, &other.col),
-        }
-    }
-
     // Apply a QuantumGate to a QuantumState
     pub fn apply_gate(self, gate: QuantumGate) -> QuantumState {
-        if self.size() != gate.size.sqrt() {
+        if gate.size == 0 {
+            return QuantumState {
+                col: self.clone().col,
+            };
+        }
+
+        if self.size() != gate.size {
             panic!(
                 "Trying to apply a gate for {} qubits to a state with {} qubits",
                 gate.size,
@@ -66,53 +51,12 @@ impl QuantumState {
 
         QuantumState { col }
     }
-
-    // Used in test to apply a QuantumGate to a specific qubit in a QuantumState
-    #[cfg(test)]
-    pub fn apply_gate_to_qubit(mut self, gate: QuantumGate, index: usize) -> QuantumState {
-        let length = self.col.len();
-
-        let no_of_qubits = self.col.len().ilog2().to_usize().unwrap();
-
-        let mut result = arr2(&[[]]);
-
-        let mut i = 0;
-        while i < no_of_qubits {
-            if i == index {
-                result = if result.len() != 0 {
-                    kron(&result, &gate.matrix)
-                } else {
-                    gate.matrix.clone()
-                };
-
-                if gate.size != 2 {
-                    let n_of_spaces_filled = gate.size / 2;
-                    i += n_of_spaces_filled - 1;
-                }
-            } else {
-                result = if result.len() != 0 {
-                    kron(&result, &QuantumGate::i_gate().matrix)
-                } else {
-                    QuantumGate::i_gate().matrix
-                };
-            }
-
-            i += 1;
-        }
-
-        if length == result.len().sqrt() {
-            self.col = result.dot(&self.col)
-        } else {
-            panic!("Error in apply_gate, matrices of wrong size!")
-        }
-
-        self
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ndarray::arr2;
 
     // Test that a simple state is correctly initialized
     #[test]
@@ -146,20 +90,5 @@ mod tests {
     fn test_size() {
         let state = QuantumState::new(5);
         assert_eq!(state.size(), 5);
-    }
-
-    // Test that the Kronecker product of two states is correct
-    #[test]
-    fn test_kronecker() {
-        let state1 = QuantumState::new(1);
-        let state2 = QuantumState::new(1);
-        let expected_state = arr2(&[
-            [Complex::new(1.0, 0.0)],
-            [Complex::new(0.0, 0.0)],
-            [Complex::new(0.0, 0.0)],
-            [Complex::new(0.0, 0.0)],
-        ]);
-        let result = state1.kronecker(&state2);
-        assert_eq!(result.col, expected_state);
     }
 }
--- backend/src/simulation/utils.rs
+++ backend/src/simulation/utils.rs
@@ -3,44 +3,6 @@ use crate::ComplexContainer;
 use ndarray::Array2;
 use num::Complex;
 
-pub fn format_complex(c: &Complex<f64>) -> String {
-    let (re, im) = (c.re, c.im);
-    let mut parts = Vec::new();
-
-    if re.abs() > 1e-10 {
-        parts.push(format_number(re));
-    }
-
-    if im.abs() > 1e-10 {
-        let sign = if im > 0.0 { "+" } else { "-" };
-        let formatted_im = format!("{} {}", sign, format_number(im.abs()));
-        parts.push(formatted_im);
-    }
-
-    if parts.is_empty() {
-        "0".to_string()
-    } else {
-        parts.join(" ")
-    }
-}
-
-pub fn format_number(num: f64) -> String {
-    let precision = 3;
-    let threshold = 1e-10;
-
-    if (num.fract()).abs() < threshold {
-        return format!("{:.0}", num);
-    }
-
-    let formatted = format!("{:.1$}", num, precision);
-
-    if (num - formatted.parse::<f64>().unwrap()).abs() > threshold {
-        format!("{}..", formatted)
-    } else {
-        formatted
-    }
-}
-
 pub fn format_to_complex_container(state: &QuantumState) -> Vec<ComplexContainer> {
     let mut container_vec = Vec::new();
     for el in &state.col {
