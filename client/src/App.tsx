48d6b406bbb3b796f364c010bdf618e961b70840
Chiara Cesarini <chiac975@gmail.com>
2024-02-16 16:09:08 +0100

    Complete refactoring

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,81 @@
 import React from 'react';
-import Toolbar from './components/toolbar';
-import Circuitboard from './components/circuitboard';
 import './App.css';
+import {DndContext} from '@dnd-kit/core';
+import {useDraggable, useDroppable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
 
 function App() {
   return (
     <div className="App">
-      <Toolbar />
-      <Circuitboard />
+      <DndContext onDragEnd={handleDragEnd}>
+        <Toolbar />
+        <Circuitboard />
+      </DndContext>
     </div>
   );
+}
+
+function Toolbar(){
+  return (<Gate name="X"/>);
+}
+
+function Circuitboard(){
+  return(
+  <div>
+    <QubitLine id="0"/>
+    <QubitLine id="1"/>
+    <QubitLine id="2"/>
+  </div>)
+}
+
+function QubitLine(props:any){
+  return (
+  <div className='qubitLine'>
+    <h2>|0⟩</h2>
+    <Slot id = {props.id + "0"}/>
+    <Slot id = {props.id + "1"}/>
+    <Slot id = {props.id + "2"}/>
+    <Slot id = {props.id + "3"}/>
+  </div>);
+}
+
+function Gate(props:any) {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+      id: props.name,
+    });
+    const style = {
+      transform: CSS.Translate.toString(transform),
+      width: 60,
+      height: 60
+    };
+    
+    return (
+      <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+        <h1>{props.name}</h1>
+      </button>
+    );
+}
+
+function Slot(props:any) {
+  const {isOver, setNodeRef} = useDroppable({
+    id: props.id,
+  });
+  const style = {
+    opacity: isOver ? 0.2 : 0.1,
+    backgroundColor: 'white',
+    width: 70,
+    height: 70
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
+  );
+}
+
+function handleDragEnd(){
+  return null;
 }
 
 export default App;
