49f1e503412f1b16d7e47f0a00c153a573b54a2e
Chiara Cesarini <chiac975@gmail.com>
2024-03-11 18:03:47 +0100

    Lite refaktorering

--- backend/src/simulation/simulator.rs
+++ backend/src/simulation/simulator.rs
@@ -39,44 +39,34 @@ pub fn simulate_circuit(incoming_data: UnparsedCircuit) -> Result<CircuitStates>
     }
 
     let circuit: ParsedCircuit = build_circuit_from_data(incoming_data);
-    
     let states: CircuitStates = simulate(circuit);
-
     Ok(states)
 }
 
 fn simulate(circuit_to_simulate: ParsedCircuit) -> CircuitStates {
-    // Every time step is a list entry and in each time step there is a list of all states corresponding to groups of entangled qubits
-    // at that time step.
-    let mut state_list: CircuitStates = vec![];
+    let mut state_list: CircuitStates = vec![initialize_qubit_states(circuit_to_simulate)];
+
+    for (index, gates_at_step) in circuit_to_simulate.circuit.into_iter().enumerate() {
+        state_at_step: QuantumStep = step(state_list - 1, gates_at_step);
+        state_list.push(state_at_step);
+    }
     
-    // TODO move this to helper function (initialize_qubit_states or something)
+    state_list
+}
+
+fn initialize_qubit_states(amount: i32) -> QuantumStep {
     let mut initial_states: QuantumStep = vec![];
-    for (i, qubit) in circuit_to_simulate.circuit[0].iter().enumerate() {
+    for i in 0..amount {
         initial_states.push(QuantumState::new(vec![i as i32]));
     }
-    state_list.push(initial_states);
+    initial_states
+}
 
-    let mut states_before_step: QuantumStep = initial_states;
-    for (index, gates_at_step) in circuit_to_simulate.circuit.into_iter().enumerate() {
-        states_before_step = state_list[index];
-        state_after_step = step(states_before_step, gates_at_step);
-        state_list.push(state_after_step);
-    }
-
-    /*let mut current_step: Vec<QuantumState> = vec![];
-    for (i, qubit) in circuit[0].iter().enumerate() {
-        current_step.push(QuantumState::new(vec![i as i32]));
-    }
-    state_list.push(current_step);
-    
-    for (step, gates_at_step) in circuit.into_iter().enumerate() {
-
-        let mut states_at_step: Vec<QuantumState> = vec![];
+fn step(states_before: QuantumStep, gates_at_step: GatesAtStep) -> QuantumStep {
+    let mut states_at_step: Vec<QuantumState> = vec![];
 
         for (qubits_in_gate, gate) in gates_at_step {
-
-            // Find all qubit groups in the previous time step that are included in the current gate and combines them into one large state
+            // Finds all qubit groups in the previous time step that are included in the current gate and combines them into one large state
             let mut state_into_gate: QuantumState;
             for quantum_state in state_list[step - 1].clone().into_iter().enumerate(){
                 if(is_unordered_sublist_of(quantum_state.qubits, qubits_in_gate)){
@@ -86,15 +76,8 @@ fn simulate(circuit_to_simulate: ParsedCircuit) -> CircuitStates {
             let mut state_after_gate: QuantumState = state_into_gate.apply_gate(gate);
             states_at_step.push(state_after_gate);
         }
-        state_list.push(states_at_step);
+    states_at_step
 
-        
-    }*/
-    state_list
-}
-
-fn step(states_before: QuantumStep, gates_at_step: GatesAtStep) -> QuantumStep {
-    // TODO
 }
 
 fn is_unordered_sublist_of(small_list: [T], large_list: [T]) -> bool {
