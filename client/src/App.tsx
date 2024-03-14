9770ff0b517040366f520d86adc5e18f28cf94b7
aalexpersson <alex.persson0408@gmail.com>
2024-03-14 13:23:45 +0100

    broke out placedgate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import './circuitboard.css';
 import './toolbar.css';
 import Toolbar from './toolbar';
+import PlacedGate from './placedGate';
 import {DndContext} from '@dnd-kit/core';
 import {useDraggable, useDroppable} from '@dnd-kit/core';
 import {CSS} from '@dnd-kit/utilities';
@@ -214,21 +215,6 @@ function App() {
       } 
     });
   }*/
-}
-
-function PlacedGate(props:any){
-  
-  // Display nothing if there is no placed gate (which is the same as the identity gate).
-  if(props.gateType != "I"){
-    return (
-      <button className = "placedGate">
-        <h1>{props.name}</h1>
-      </button>
-    );
-  } 
-  else return null;
-  
-  
 }
 
 /*function PlacedControlGate(props:any, event:any){
